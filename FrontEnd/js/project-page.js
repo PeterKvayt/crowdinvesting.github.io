76e1b21ff9611aaa230440c5da7f70d7e09764b5
PeterKvayt <peter.kvayt@gmail.com>
2019-12-01 22:45:36 +0300

    add slider for project-page.html

---
+++ FrontEnd/js/project-page.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  // показать/скрыть тему/вопрос
+  $('.accordion-btn, .accordion-btn-alt').on('click', function(){
+    let content = $(this).next();
+    if(content.css('display') != 'block'){
+      content.slideDown(200);
+      $(this).children().first().attr('class', 'fa fa-times fa-fw');
+      // $(this).css('color', 'var(--greenPoles)');
+    }
+    else{
+      content.slideUp(200);
+      $(this).children().first().attr('class', 'fa fa-plus fa-fw');
+      // $(this).css('color', 'var(--borderBtn)');
+    }
+  });
+});
---
+++ FrontEnd/js/slick-slider.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $('.slider-box').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    arrows:true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          arrows:true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
