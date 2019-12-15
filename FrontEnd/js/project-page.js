fad8199db25d37fad5ff016cf5d3ffd030e8c9bd
PeterKvayt <peter.kvayt@gmail.com>
2019-12-15 17:32:13 +0300

    переделал тест и страницу проекта

--- FrontEnd/js/project-page.js
+++ FrontEnd/js/project-page.js
@@ -1,16 +1,50 @@
 $(document).ready(function(){
+  let profit  = document.getElementById('profit').firstChild.data.trim();
+  let financialAim = document.getElementById('financial-aim').firstChild.data.trim();
+
+  // устанавливает пробелы
+  document.getElementById('profit').firstChild.data = SetSpace(profit);
+  document.getElementById('financial-aim').firstChild.data = SetSpace(financialAim);
+
+  let progress = profit / financialAim * 100;
+  progress = progress.toFixed(2);
+  // задает ширину прогресс бара
+  $('.progress-bar').width(progress+'%');
+  // устанавливаем прогресс в процентах
+  document.getElementById('profit-value').innerHTML = progress+'%';
+
   // показать/скрыть тему/вопрос
-  $('.accordion-btn, .accordion-btn-alt').on('click', function(){
-    let content = $(this).next();
-    if(content.css('display') != 'block'){
-      content.slideDown(200);
-      $(this).children().first().attr('class', 'fa fa-times fa-fw');
-      // $(this).css('color', 'var(--greenPoles)');
+  $('.accordion-section').on('click', '.accordion-btn', function(event){
+    let target = $(event.target);
+    if (target.next().css('display') != 'block') {
+      target.next().slideDown(200);
+      target.children().first().attr('class', 'fa fa-times fa-fw');
     }
     else{
-      content.slideUp(200);
-      $(this).children().first().attr('class', 'fa fa-plus fa-fw');
-      // $(this).css('color', 'var(--borderBtn)');
+      target.children().first().attr('class', 'fa fa-plus fa-fw');
+      target.next().slideUp(200);
     }
-  });
+
+    let parent = event.target.parentNode;
+    let articles = $(parent).children('.accordion-btn');
+    for (let i = 0; i < articles.length; i++) {
+      if (i != (target.index())/2) {
+        $(articles[i]).children().first().attr('class', 'fa fa-plus fa-fw');
+        $(articles[i]).next().slideUp(200);
+      }
+    }
+
+    
+  })
+
+  // задает пробелы между числами
+  function SetSpace(str){
+    let result = str;
+    for (let i = str.length - 2; i >= 0; i--) {
+      if(i % 3 == 0){
+        result = result.substr(0, i) + result[i]+' '+ result.substr(i + 1);
+      }      
+    }
+    return result;
+  }
 });
--- FrontEnd/js/test.js
+++ FrontEnd/js/test.js
@@ -39,8 +39,8 @@ $(document).ready(function(){
     let parent = event.target.parentNode.parentNode;
     let chekcs = $(parent).children('p').children('.fa');
     for (let index = 0; index < chekcs.length; index++) {
-      chekcs[index].className = 'fa fa-square';
+      chekcs[index].className = 'fa fa-square fa-2x answer-check';
     }
-    event.target.className = 'fa fa-check-square';
+    event.target.className = 'fa fa-check-square fa-2x answer-check';
   })
 });
