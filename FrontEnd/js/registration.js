b0a159bc190f280a3f75576c84d4ead7bc77bb19
PeterKvayt <peter.kvayt@gmail.com>
2020-01-07 14:10:04 +0300

    fix checks in registration.html

--- FrontEnd/js/registration.js
+++ FrontEnd/js/registration.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-  $('.registration-check').on('click','.fa-square', function(event){
+  $('.registration-check').on('click', function(event){
     let chekcs = $('.fa-check-square');
     chekcs.each(function(index){
       this.className = 'fa fa-square fa-lg';
-      console.log('clear');
+      // console.log('clear');
     })
-    event.target.className = 'fa fa-check-square fa-lg';
+    if(event.target.className.includes('square')){
+      event.target.className = 'fa fa-check-square fa-lg';
+    }
+    else{
+      $(event.target).prev().attr('class', 'fa fa-check-square fa-lg');
+    }
   })
 });
